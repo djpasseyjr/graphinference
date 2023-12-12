c16d4bfac3bce4afbd7cfe6b7052ba667ba2342b
DJ Passey <djpasseyjr@gmail.com>
2023-12-12 03:50:09 -0500

    bug fixes

--- scripts/job_script_noise_exp_test.sh
+++ scripts/job_script_noise_exp_test.sh
@@ -1,3 +1,4 @@
-source /nas/longleaf/home/djpassey/graphinference/graphinference_venv/bin/activate
+module purge
+module load python/3.9.6
 
 python /nas/longleaf/home/djpassey/graphinference/experiments/noise_experiments_test.py $1
