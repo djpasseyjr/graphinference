ea3d8801ed38bf5b43ec9e182ca4195fd46172d6
DJ Passey <djpasseyjr@gmail.com>
2023-12-12 03:52:42 -0500

    add pip installs

--- scripts/job_script_noise_exp_test.sh
+++ scripts/job_script_noise_exp_test.sh
@@ -1,4 +1,6 @@
 module purge
 module load python/3.9.6
+pip install sktime torch pysindy
+pip install /nas/longleaf/home/djpassey/graphinference/
 
 python /nas/longleaf/home/djpassey/graphinference/experiments/noise_experiments_test.py $1
