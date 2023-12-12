07ba4233425fbc6a5eee6ae9f6ddb143215578a6
DJ Passey <djpasseyjr@gmail.com>
2023-12-12 03:21:48 -0500

    debugging conda

--- scripts/job_script_noise_exp_test.sh
+++ scripts/job_script_noise_exp_test.sh
@@ -1,4 +1,3 @@
-conda deactivate
 source /nas/longleaf/home/djpassey/graphinference/graphinference_venv/bin/activate
 
 python /nas/longleaf/home/djpassey/graphinference/experiments/noise_experiments_test.py $1
