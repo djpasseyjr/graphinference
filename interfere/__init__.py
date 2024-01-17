f192f60911d5d27fb1f7d41acab1d17e87f2c63f
DJ Passey <djpasseyjr@gmail.com>
2024-01-17 15:49:47 -0500

    removed interfere package

--- interfere/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import (
-    DynamicModel, 
-    generate_counterfactual_dynamics,
-)
-
-from . import dynamics
-from . import methods
-from .interventions import perfect_intervention, signal_intervention, PerfectIntervention
-from . import benchmarking
